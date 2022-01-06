1a508ba55ca7c9f667034ac0767a18e6dd6a6b4f
Joanna Chądzyńska <jmalek87@gmail.com>
2022-01-06 20:25:39 +0100

    update app

---
+++ projects/shared-lib/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of shared-lib
+ */
+
+export * from './lib/logo/logo.component';
+export * from './lib/shared-lib.component';
+export * from './lib/shared-lib.module';
+export * from './lib/shared-lib.service';
+
--- projects/space-app/src/app/app.module.ts
+++ projects/space-app/src/app/app.module.ts
@@ -1,30 +1,26 @@
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { A2sCommModule } from 'a2s-comm';
 import { API_URL } from 'space-api/tokens';
 import { environment } from '../environments/environment';
-
+import { SharedLibModule } from './../../../shared-lib/src/lib/shared-lib.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
 import { StartComponent } from './views/start/start.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StartComponent
-  ],
+  declarations: [AppComponent, StartComponent],
   imports: [
     A2sCommModule,
     BrowserModule,
     SharedModule,
+    SharedLibModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [
-    {provide: API_URL, useValue: environment.apiUrl}
-  ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: API_URL, useValue: environment.apiUrl }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
