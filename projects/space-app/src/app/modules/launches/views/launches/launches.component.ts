c510da94a7e3593348de810f6e81c32ec22d32b0
Joanna Chądzyńska <jmalek87@gmail.com>
2022-01-07 17:51:50 +0100

    update launches view

--- projects/space-app/src/app/modules/launches/views/launches/launches.component.ts
+++ projects/space-app/src/app/modules/launches/views/launches/launches.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { LaunchesService } from 'space-api/services';
+import { LaunchDetailsUpdate } from 'space-api/types';
 
 @Component({
   selector: 'app-launches',
   templateUrl: './launches.component.html',
-  styleUrls: ['./launches.component.scss']
+  styleUrls: ['./launches.component.scss'],
 })
-export class LaunchesComponent implements OnInit {
+export class LaunchesComponent {
   launches = this.launchesService.getLaunches();
 
-  constructor(private launchesService: LaunchesService) { }
+  constructor(private launchesService: LaunchesService) {}
 
-  ngOnInit(): void {
+  updateLaunchDetails(detailsUpdate: LaunchDetailsUpdate): void {
+    this.launchesService.updateDetails(detailsUpdate).subscribe(() => {
+      this.launches = this.launchesService.getLaunches();
+    });
   }
-
 }
